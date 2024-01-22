3c80c49071a7fb44d4b0f45d098e8b6b4ff8fb37
Anuj Panthri <panthrianuj@gmail.com>
2024-01-22 11:27:37 +0530

    fixed logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var isValid = true;
 
-function setError(input_elem,msg){
+function setError(input_elem, msg) {
     var input_container = input_elem.closest(".input_container");
     input_container.classList.add("error");
     error_msg_elem = input_container.querySelector('.error_msg');
@@ -8,91 +8,90 @@ function setError(input_elem,msg){
     error_msg_elem.innerText = msg;
 }
 
-function removeError(input_elem){
+function removeError(input_elem) {
     var input_container = input_elem.closest(".input_container");
     input_container.classList.remove("error");
     error_msg_elem = input_container.querySelector('.error_msg');
     error_msg_elem.innerText = "";
 }
 
-function validate_isEmpty(elem){
-    if(elem.value==""){
-        setError(elem,"This field is required")
+function validate_isEmpty(elem) {
+    if (elem.value == "") {
+        setError(elem, "This field is required")
         isValid = false;
         return false;
     }
     return true;
 }
 
-function validate_day(elem){
+function validate_day(elem) {
 
-    if(!validate_isEmpty(elem)) return false;
+    if (!validate_isEmpty(elem)) return false;
     var day = parseInt(elem.value);
-    if(day<1 || day>31){
-        setError(elem,"Must be a valid day");
+    if (day < 1 || day > 31) {
+        setError(elem, "Must be a valid day");
         isValid = false;
         return false;
     }
     return true;
 }
 
-function validate_month(elem){
+function validate_month(elem) {
 
-    if(!validate_isEmpty(elem)) return false;
+    if (!validate_isEmpty(elem)) return false;
     var month = parseInt(elem.value);
-    if(month<1 || month>12){
-        setError(elem,"Must be a valid month");
+    if (month < 1 || month > 12) {
+        setError(elem, "Must be a valid month");
         isValid = false;
         return false;
     }
     return true;
 }
 
-function validate_year(elem){
+function validate_year(elem) {
 
-    if(!validate_isEmpty(elem)) return false;
+    if (!validate_isEmpty(elem)) return false;
     var year = parseInt(elem.value);
-    if(year<1){
-        setError(elem,"Must be a valid year");
+    if (year < 1) {
+        setError(elem, "Must be a valid year");
         isValid = false;
         return false;
     }
     return true;
 }
 
-function validate_old_date(day,month,year){
+function validate_old_date(day, month, year) {
     // check if date is older than today or equal
     var dobdate = new Date(`${year.value}-${parseInt(month.value)}-${day.value}`);
 
     var currdate = new Date();
-    currdate = new Date(`${currdate.getFullYear()}-${currdate.getMonth()+1}-${currdate.getDate()}`);
+    currdate = new Date(`${currdate.getFullYear()}-${currdate.getMonth() + 1}-${currdate.getDate()}`);
     // console.log(dobdate);
     // console.log(currdate);
-    if(dobdate.getTime()>currdate.getTime()){
+    if (dobdate.getTime() > currdate.getTime()) {
         isValid = false;
-        setError(day,"must be in past");
-        setError(month,"must be in past");
-        setError(year,"must be in past");
+        setError(day, "must be in past");
+        setError(month, "must be in past");
+        setError(year, "must be in past");
         return false;
     }
     return true;
 }
 
-function validate_valid_date(day,month,year){
+function validate_valid_date(day, month, year) {
     // console.log(parseInt(day.value));
     // console.log(num_of_day_in_month(parseInt(year.value),parseInt(month.value)));
-    if(parseInt(day.value)>num_of_day_in_month(parseInt(year.value),parseInt(month.value)))
-    {
+    if (parseInt(day.value) > num_of_day_in_month(parseInt(year.value), parseInt(month.value))) {
         isValid = false;
-        setError(day,"Invalid date");
-        setError(month,"Invalid date");
-        setError(year,"Invalid date");
+        setError(day, "Invalid date");
+        setError(month, "Invalid date");
+        setError(year, "Invalid date");
         return false;
     }
     return true;
 }
 
-function validate(form){
+function validate(form) {
     removeError(form.day);
     removeError(form.month);
     removeError(form.year);
@@ -101,60 +100,73 @@ function validate(form){
     validate_day(form.day);
     validate_month(form.month);
     validate_year(form.year);
-    validate_valid_date(form.day,form.month,form.year);
-    validate_old_date(form.day,form.month,form.year);
+    validate_valid_date(form.day, form.month, form.year);
+    validate_old_date(form.day, form.month, form.year);
     return true;
 }
 
-function calculate_age(ev,form){
+function calculate_age(ev, form) {
     ev.preventDefault();
 
     // reset back to default;
-    age_y.innerText =  '--'; 
-    age_m.innerText =  '--'; 
-    age_d.innerText =  '--'; 
+    age_y.innerText = '--';
+    age_m.innerText = '--';
+    age_d.innerText = '--';
 
     validate(form)
-    if(!isValid) return;
+    if (!isValid) return;
 
     // calculate age
     var day = parseInt(form.day.value);
     var month = parseInt(form.month.value);
     var year = parseInt(form.year.value);
-    
+
     // get current date
     var date = new Date();
 
-    var currYear = date.getUTCFullYear(); 
-    var currMonth = (date.getUTCMonth()+1);     // date.getUTCMonth() gives month in 0 to 11
-    var currDay = date.getDate(); 
+    var currYear = date.getUTCFullYear();
+    var currMonth = (date.getUTCMonth() + 1);     // date.getUTCMonth() gives month in 0 to 11
+    var currDay = date.getDate();
+
+    if (currMonth < month) {
+        currYear -= 1;
+        currMonth += 12;
+    }
+
+    if (currDay < day) {
+        currMonth -= 1;
+
+        // if currMonth if smaller than 1 =>{0,-1,-2} then take an year back to add 12 month
+        if (currMonth < 1) {
+            currYear -= 1;
+            currMonth += 12;
+        }
+        
+        // renormalize under 1-12
+        if (currMonth > 12) {
+            currMonth = (currMonth % 12);
+        }
+
+        currDay += num_of_day_in_month(currYear,currMonth);
+
+    }
+    console.log(currDay, currMonth, currYear);
 
     var ageYear = currYear - year;
-    var ageMonth = currMonth - month; 
+    var ageMonth = currMonth - month;
     var ageDay = currDay - day;
 
-    // console.log(ageDay);
-
-    if(ageDay<0){   // if negative
-        ageMonth-=1;
-        ageDay = ageDay+num_of_day_in_month(year+ageYear-1,month); // add birthday month(last year's)
-    }
-    if(ageMonth<0){   // if negative
-        ageYear-=1;
-        ageMonth += 12; // add an year
-    }
-
-    age_y.innerText =  ageYear.toString(); 
-    age_m.innerText =  ageMonth.toString(); 
-    age_d.innerText =  ageDay.toString(); 
+    age_y.innerText = ageYear.toString();
+    age_m.innerText = ageMonth.toString();
+    age_d.innerText = ageDay.toString();
 
 }
 
 
-function is_leap(year){
+function is_leap(year) {
     // every year that is divisible by 4 is a leap year except for the year divisible by 100
     // 
-    if ((year%4==0 && year%100!=0) || (year%4==0 && year%400==0)){
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 400 == 0)) {
         // if year is divisible with 4 and is non centurial year
         // if year is divisible with 4 and is centurial year and is divisible by 400
         return true;
@@ -162,10 +174,10 @@ function is_leap(year){
     return false;
 }
 
-function num_of_day_in_month(year,month){
+function num_of_day_in_month(year, month) {
     // month num like 1 for jan , 2 for feb
 
-    switch(month){
+    switch (month) {
         case 1:
         case 3:
         case 5:
@@ -175,19 +187,20 @@ function num_of_day_in_month(year,month){
         case 12:
             return 31;
             break;
+            
         case 4:
         case 6:
         case 9:
         case 11:
             return 30;
             break;
-        
+
         case 2:
             // check if leap
-            if(is_leap(year)){
+            if (is_leap(year)) {
                 return 29;
             }
-            else{
+            else {
                 return 28;
             }
 
